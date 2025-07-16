a4a7e3547800d0626ff815a708edb9066722be99
Berliana-Novianti <berliananovianti43@gmail.com>
2025-07-16 11:00:11 +0700

    mengembalikan struktur laravel standar

--- Modules/Agenda/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-agenda::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('agenda.name') !!}</p>
-</x-agenda::layouts.master>
--- Modules/Agenda/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Agenda\Http\Controllers\AgendaController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('agendas', AgendaController::class)->names('agenda');
-});
