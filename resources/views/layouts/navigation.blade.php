6e11f98de80f4baffa2f5dbc24199ceed045e82d
Berliana-Novianti <berliananovianti43@gmail.com>
2025-07-21 14:12:57 +0700

    merge branch zidan ke berliana

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Dasbor') }}
                     </x-nav-link>
                     <x-nav-link :href="route('agenda-harian.index')" :active="request()->routeIs('agenda-harian.*')">
                         {{ __('Agenda Harian') }}
