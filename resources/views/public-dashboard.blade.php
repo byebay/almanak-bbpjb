5d55ea1c7fb5bfb5ecca980cc67a1821a1c20432
ZidanTSM22 <zidan.tri.tif23@polban.ac.id>
2025-09-08 08:51:49 +0700

    implementasi fitur tambahan informasi kunjungan publik

--- resources/views/public-dashboard.blade.php
+++ resources/views/public-dashboard.blade.php
@@ -46,6 +46,9 @@
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5cmZSSbEF_Aww6083_87Z96cCKwM6CA0ww&s" alt="Logo Balai Bahasa" class="block h-12 w-auto">
                     <span class="font-semibold text-xl text-gray-800 ml-3">Almanak</span>
                 </div>
+                <div class="hidden sm:flex items-center text-sm text-gray-500">
+                        <span>Pengunjung Bulan Ini: <strong class="text-gray-800">{{ $visitorCount }}</strong></span>
+                    </div>
                 <!-- Tombol Login -->
                 <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700">
                     Login Pegawai
