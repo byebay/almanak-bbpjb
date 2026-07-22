822ad2ff046bac6e55497d7143734def26423962
hanif13579 <hanifidin.ibrahim.tif24@polban.ac.id>
2026-07-22 13:44:32 +0700

    feat: Menampilkan diagram batang statistik pengunjung

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     // Dashboard internal juga butuh data event
     Route::get('/dashboard/events', [DashboardController::class, 'getEvents'])->name('dashboard.events');
+    Route::post('/dashboard/events', [DashboardController::class, 'storeEvent'])->name('dashboard.events.store');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
